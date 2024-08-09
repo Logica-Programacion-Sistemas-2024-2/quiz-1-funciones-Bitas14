0ae0866240e2e9bb912157547997e463f830a362
Bitas14 <sebastianfm24@gmail.com>
2024-08-09 14:02:30 -0500

    Update Quiz App.java

--- app/src/main/java/quiz1sistemasg2/App.java
+++ app/src/main/java/quiz1sistemasg2/App.java
@@ -3,14 +3,106 @@
  */
 package quiz1sistemasg2;
 
+import java.util.Scanner;
+
 public class App {
+    public int calcularValorHora (String tipoVehiculo, int HorasPArqueadero) {
+        try{ 
+            tipoVehiculo = Integer.parseInt(tipoVehiculo);
+            switch (tipoVehiculo) {
+                case "C":
+                    return 5000;
+                    break;
+                    
+            }
+            
+            switch (tipoVehiculo) {
+                case "M":
+                    return 3000;
+                    break;
+                
+            }
+            switch (tipoVehiculo) {
+                case "B":
+                return 1000;
+                break;
+            }
+            return -1;
+
+            }catch (Exception e){
+                return -1;
+            }
+        }
+    
+ 
+    public static void main(String[] args) {
+        try{
+           
+            
+        
+           String TarifaBase = "";
+            double vehiculoEntra = 0; 
+            String tipoVehiculo = "";
+            String Estudiante = "";
+            int HorasPArqueadero = 0;
+            int montoDelParqueadero = 0; 
+            int PrecioParqueadero = 0;
+
+            double descuento;
+            switch (Estudiante) {
+                case "C":
+                if (PrecioParqueadero == 5000)descuento = 0.05;
+                else if(PrecioParqueadero == 5000)descuento = 0.30;
+                           
+                break;
+                }
+            switch (Estudiante) {
+                case "M":
+                if (PrecioParqueadero == 3000)descuento = 0.05;
+                else if (PrecioParqueadero == 3000)descuento = 0.30;
+                    break;
+                switch (Estudiante) {
+                    case "B":
+                     if (PrecioParqueadero == 1000)descuento = 0.05; 
+                        else if (PrecioParqueadero == 1000)descuento = 0.30;
+                        break;
+                }
+                return valor PrecioParqueadero * descuento;
+            }
+
+            
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ingerese C para carro, Ingrese M para moto, Ingrese B para biciclieta");
+            tipoVehiculo = sc.nextLine();
+            System.out.println("Calcular horas en el parqueadero");
+            HorasPArqueadero = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Ingrese 1 si es estudiante, ingrese 2 si no");
+            Estudiante = sc.nextLine();
+            
+
+
+            }
+
+
+
+
+        }
+        
+        
+        
+            
+        
+
+
+
+
+
+        }
     
 
-    public static void main(String[] args) {
-        
-       
-
-    }
+    
 
     /*  Implemente una funcion llamada calcularValorHora que cumpla con las siguientes caracteristicas
         * Recibe: 
@@ -21,7 +113,7 @@ public class App {
         * Controlar errores retornando -1
     */
     //------------------------------------------------------------------
-    
+
 
 
     //------------------------------------------------------------------
@@ -53,4 +145,4 @@ public class App {
 
 
     //------------------------------------------------------------------
-}
+
